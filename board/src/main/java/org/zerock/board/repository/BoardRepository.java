e0f1de38f637afa4c9ba72233cd457488d9786d1
tjdals3862 <tjdals3862@naver.com>
2023-02-20 15:50:11 +0900

    20230220

---
+++ board/src/main/java/org/zerock/board/repository/BoardRepository.java
@@ -0,0 +1,27 @@
+package org.zerock.board.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.zerock.board.entity.Board;
+
+import java.util.List;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    @Query("select b, w from Board b left join b.writer w where b.bno =:bno")
+    Object getBoardWithWriter(@Param("bno") Long bno);
+
+    @Query("Select b ,r From Board b LEFT JOIN Reply r ON r.board = b WHERE b.bno =:bno")
+    List<Object[]> getBoardWithReply(@Param("bno") Long bno);
+
+    @Query(value = "Select b, w, count(r) " +
+            " From Board b " +
+            " LEFT JOIN b.writer w"+
+            " LEFT JOIN Reply r ON r.board = b" +
+            " GROUP BY b",
+            countQuery = "SELECT count(b) FROM Board b")
+    Page<Object[]> getBoardWithReplyCount(Pageable pageable);
+}
